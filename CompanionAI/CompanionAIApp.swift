496bc6608e78b00c1a881f42b5e71cd8a3d2f0df
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2023-01-19 06:09:31 +0100

    Initial Commit

---
+++ CompanionAI/CompanionAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompanionAIApp.swift
+//  CompanionAI
+//
+//  Created by Igor Tarasenko on 19/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CompanionAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
