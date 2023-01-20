0289bd0741112073145a99fe3b8a43f2a8c1b129
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2023-01-20 15:34:19 +0100

    Add chat client
    This commit adds a ChatClient struct to handle conversation logic

---
+++ Sources/ChatClient.swift
@@ -0,0 +1,75 @@
+import Foundation
+import Dependencies
+import OpenAI
+
+public struct ChatClient {
+  public var generateAnswerForConversation: (Conversation) async throws -> String
+}
+
+public enum ChatClientError: Error {
+  case noChoices
+}
+
+extension ChatClient: DependencyKey {
+  public static let liveValue: Self = {
+    let openAI = OpenAI(apiToken: ProcessInfo.processInfo.environment["OPENAI_API_KEY"]!)
+
+    return Self(
+      generateAnswerForConversation: { conversation in
+        let chat = conversation.messages
+          .map { $0.participant.name + ": " + $0.text }
+          .joined(separator: "\n\n")
+
+        let prompt =
+          """
+          The following is a conversation with a product manager(PM) for a mobile application. The product manager is helpful, creative, clever, and very friendly.
+
+          Client: Hello, who are you?
+          PM: As a product manager, I am ready to take on the development of a new mobile app. I will be provided with a brief description of the app's features and target audience, and it is my responsibility to create a detailed plan for the app's development. This will include a marketing strategy, a development schedule, and a list of potential monetization options. I will also identify potential features and improvements that can be added to the app in the future. Additionally, I will be able to recognize potential risks and come up with a plan to mitigate them. My approach will be to provide clear and concise responses without any additional explanations. What will be my first task?
+          \(chat)
+
+          \(conversation.bot.name):
+          """
+        let query = OpenAI.CompletionsQuery(
+          model: .textDavinci_003,
+          prompt: prompt,
+          temperature: 0.7,
+          max_tokens: 700,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0
+        )
+
+        let result = try await openAI.completions(query: query)
+
+        if let text = result.choices.first?.text {
+          return text
+        } else {
+          throw ChatClientError.noChoices
+        }
+      }
+    )
+  }()
+}
+
+extension DependencyValues {
+  var chatClient: ChatClient {
+    get { self[ChatClient.self] }
+    set { self[ChatClient.self] = newValue }
+  }
+}
+
+private extension OpenAI {
+  func completions(query: CompletionsQuery) async throws -> CompletionsResult {
+    try await withCheckedThrowingContinuation { continuation in
+      completions(query: query) { result in
+        switch result {
+        case let .success(result):
+          continuation.resume(returning: result)
+        case let .failure(error):
+          continuation.resume(throwing: error)
+        }
+      }
+    }
+  }
+}
--- Sources/ChatView.swift
+++ Sources/ChatView.swift
@@ -1,31 +1,6 @@
 import AppDevUtils
 import Inject
-import OpenAI
 import SwiftUI
-
-// MARK: - TextCompletion
-
-struct TextCompletion: Codable {
-  let id: String
-  let object: String
-  let created: Int
-  let model: String
-  let choices: [Choice]
-  let usage: Usage
-
-  struct Choice: Codable {
-    let text: String
-    let index: Int
-    let logprobs: String?
-    let finish_reason: String
-  }
-
-  struct Usage: Codable {
-    let prompt_tokens: Int
-    let completion_tokens: Int
-    let total_tokens: Int
-  }
-}
 
 // MARK: - ChatViewModel
 
@@ -35,7 +10,7 @@ class ChatViewModel: ObservableObject {
   @Published var currentInput: String = ""
   @Published var isLoading: Bool = false
 
-  var openAI: OpenAI { Dependencies.openAI }
+  var chatClient: ChatClient { ChatClient.liveValue }
 
   func sendButtonTap() {
     guard !currentInput.isEmpty, !isLoading else { return }
@@ -50,50 +25,28 @@ class ChatViewModel: ObservableObject {
     }
   }
 
-  func send(_ message: String) async {
+  private func send(_ message: String) async {
     sendMessage(message)
     await askDavinci()
   }
 
-  func sendMessage(_ message: String) {
+  private func sendMessage(_ message: String) {
     let newMessage = Message(text: message, participant: .user(conversation.user), timestamp: Date().timeIntervalSince1970)
     conversation.messages.append(newMessage)
   }
 
-  func receiveMessage(_ message: String) {
+  private func receiveMessage(_ message: String) {
     let newMessage = Message(text: message, participant: .bot(conversation.bot), timestamp: Date().timeIntervalSince1970)
     conversation.messages.append(newMessage)
   }
 
   private func askDavinci() async {
-    let chat = conversation.messages
-      .map { $0.participant.name + ": " + $0.text }
-      .joined(separator: "\n\n")
-
-    let prompt =
-      """
-      The following is a conversation with a product manager(PM) for a mobile application. The product manager is helpful, creative, clever, and very friendly.
-
-      Client: Hello, who are you?
-      PM: As a product manager, I am ready to take on the development of a new mobile app. I will be provided with a brief description of the app's features and target audience, and it is my responsibility to create a detailed plan for the app's development. This will include a marketing strategy, a development schedule, and a list of potential monetization options. I will also identify potential features and improvements that can be added to the app in the future. Additionally, I will be able to recognize potential risks and come up with a plan to mitigate them. My approach will be to provide clear and concise responses without any additional explanations. What will be my first task?
-      \(chat)
-
-      \(conversation.bot.name):
-      """
-    let query = OpenAI.CompletionsQuery(
-      model: .textDavinci_003,
-      prompt: prompt,
-      temperature: 0.7,
-      max_tokens: 700,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0
-    )
-    let result = try! await openAI.completions(query: query)
-    if let text = result.choices.first?.text {
-      receiveMessage(text)
-    } else {
-      log("No choices")
+    do {
+      let answer = try await chatClient.generateAnswerForConversation(conversation)
+      receiveMessage(answer)
+    } catch {
+      receiveMessage("I'm sorry, I'm having trouble understanding you. Can you rephrase?")
+      log.error(error)
     }
   }
 }
@@ -169,20 +122,5 @@ struct ChatView: View {
     }
     .padding(.grid(2))
     .enableInjection()
-  }
-}
-
-private extension OpenAI {
-  func completions(query: CompletionsQuery) async throws -> CompletionsResult {
-    try await withCheckedThrowingContinuation { continuation in
-      completions(query: query) { result in
-        switch result {
-        case let .success(result):
-          continuation.resume(returning: result)
-        case let .failure(error):
-          continuation.resume(throwing: error)
-        }
-      }
-    }
   }
 }
