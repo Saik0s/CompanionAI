5c9ca398e7727ef75097ebbd7ee166a193201c55
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2023-01-20 15:18:13 +0100

    Update package dependencies and project settings
    This commit updates the package.json file to add new dependencies, adds a setting for GCC_WARN_INHIBIT_ALL_WARNINGS, and removes Base.xcconfig from the project settings. It also updates source paths in graph.json to match the current directory structure.

--- Makefile
+++ Makefile
@@ -1,3 +1,15 @@
 all:
 	tuist fetch
 	tuist generate --no-open --no-cache
+
+build_debug:
+	tuist build --generate --configuration Debug --build-output-path .build/
+	cp -r .build/Debug/CompanionAI.app CompanionAI_debug.app
+
+build_release:
+	tuist build --generate --configuration Release --build-output-path .build/
+	cp -r .build/Release/CompanionAI.app CompanionAI.app
+
+run:
+	tuist build --generate --configuration Debug --build-output-path .build/
+	.build/Debug/CompanionAI.app/Contents/MacOS/CompanionAI
--- Project.swift
+++ Project.swift
@@ -2,6 +2,15 @@ import Foundation
 import ProjectDescription
 import ProjectDescriptionHelpers
 
+let projectSettings: SettingsDictionary = [
+  "GCC_TREAT_WARNINGS_AS_ERRORS": "YES",
+  "SWIFT_TREAT_WARNINGS_AS_ERRORS": "YES",
+  "OTHER_SWIFT_FLAGS[config=Debug][sdk=*][arch=*]": "-D DEBUG $(inherited) -Xfrontend -warn-long-function-bodies=500 -Xfrontend -warn-long-expression-type-checking=500 -Xfrontend -debug-time-function-bodies -Xfrontend -enable-actor-data-race-checks",
+  "OTHER_LDFLAGS[config=Debug][sdk=*][arch=*]": "$(inherited) -Xlinker -interposable",
+  "CODE_SIGN_IDENTITY": "",
+  "CODE_SIGNING_REQUIRED": "NO",
+]
+
 func target(name: String) -> Target {
   Target(
     name: name,
@@ -33,11 +42,9 @@ let project = Project(
       tabWidth: 2
     )
   ),
-  settings: .settings(
-    configurations: [
-      .debug(name: "Debug", xcconfig: "//configs/Base.xcconfig"),
-      .release(name: "Release", xcconfig: "//configs/Base.xcconfig"),
-    ]
-  ),
+  settings: .settings(configurations: [
+    .debug(name: "Debug", settings: projectSettings, xcconfig: nil),
+    .release(name: "Release", settings: projectSettings, xcconfig: nil),
+  ]),
   targets: [target(name: "CompanionAI")]
 )
